a0a28390dec67ac1e77cbde705346aa5689cc599
xushanfeng <xushanfeng@finlink.net.cn>
2017-06-16 15:19:35 +0800

    test

---
+++ Work/test_A.py
@@ -0,0 +1,10 @@
+class Test_A:
+
+    def setup(self):
+        pass
+
+    def test_a(self):
+        assert 1 == 1
+
+    def teardown(self):
+        pass
---
+++ Work/test_B.py
@@ -0,0 +1,10 @@
+class Test_B:
+
+    def setup(self):
+        pass
+
+    def test_b(self):
+        assert 1 == 1
+
+    def teardown(self):
+        pass
