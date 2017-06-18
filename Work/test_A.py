b130eca372d03da9ba8d89e497e11692941e8490
xushanfeng <xushanfeng@finlink.net.cn>
2017-06-18 14:11:48 +0800

    params

--- Work/test_A.py
+++ Work/test_A.py
@@ -1,9 +1,13 @@
+import pytest
 class Test_A:
 
     def setup(self):
         pass
 
-    def test_a(self):
+
+    @pytest.mark.parametrize("a",[(111),(2222)])
+    def test_a(self, a):
+        print a
         assert 1 == 1
 
     def teardown(self):
