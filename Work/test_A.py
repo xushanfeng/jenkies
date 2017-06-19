e67fccf7b6a3bd4c5671705167460a4bd5c20e67
xushanfeng <xushanfeng@finlink.net.cn>
2017-06-19 10:05:00 +0800

    params

--- Work/test_A.py
+++ Work/test_A.py
@@ -9,7 +9,7 @@ class Test_A:
     @pytest.mark.parametrize("msg,para",[("输入",1111),("输出",2222)])
     def test_a(self, msg,para):
         print msg+":",para
-        assert 1 == 1
+        assert 1 == 2
 
     def teardown(self):
         pass
