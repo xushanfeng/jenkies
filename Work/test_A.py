5b4d5ab54d26add6c5dc8e5d23b132287bcd5664
xushanfeng <xushanfeng@finlink.net.cn>
2017-08-04 21:30:30 +0800

    level

--- Work/test_A.py
+++ Work/test_A.py
@@ -32,6 +32,7 @@ class Test_A:
         allure.attach("描述","this a error case!")
         assert 1 == 2
 
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test_right_case(self):
         allure.attach("描述","this a right case!")
         assert 1 == 1
