4e890262559fb93fc6a53b67be7d325856266c69
xushanfeng <xushanfeng@finlink.net.cn>
2017-08-07 14:08:28 +0800

    all kind of type

--- Work/test_A.py
+++ Work/test_A.py
@@ -18,29 +18,29 @@ class Test_A:
     @pytest.mark.parametrize("ss, sdfa", [(1, 2), (2, 3)])
     @pytest.allure.severity(pytest.allure.severity_level.MINOR)
     def test_create_channel_1(self, ss, sdfa):
-        allure.attach('描述', '这是一个注册登录的case')
-        f = open("Work/WechatIMG724.PNG",'r').read()
-        allure.attach("创建测试渠道：%s"%str(ss),f, allure.attach_type.PNG)
-        self.a(ss, sdfa)
+        allure.attach('描述', self.a(ss, sdfa))
+        f = open("Work/WechatIMG724.PNG", 'r').read()
+        allure.attach("创建测试渠道：%s"%str(ss), f, allure.attach_type.PNG)
+
 
     @allure.step(title="登录账号:{1},注册:{2}")
     # @allure.step(title="注册:{2}")
-    def a(self, balck, test):
-        return balck,test
+    def a(self, black, test):
+        return black, test
 
     @pytest.allure.severity(pytest.allure.severity_level.MINOR)
     def test_error_case(self):
-        allure.attach("描述","this a error case!")
+        allure.attach("描述", "this a error case!")
         assert 1 == 2
 
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test_right_case(self):
-        allure.attach("描述","this a right case!")
+        allure.attach("描述", "this a right case!")
         assert 1 == 1
 
     @pytest.allure.severity(pytest.allure.severity_level.TRIVIAL)
     def test_trivial_case(self):
-        allure.attach("trivial","this is a trivial case!")
+        allure.attach("trivial", "this is a trivial case!")
 
     @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
     def test_block_case(self):
